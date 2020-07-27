2238786cefe3411b0d544e4263a9054d684c11d3
KIL Bismuth <kiralowrey@gmail.com>
2020-07-26 18:29:53 -0700

    created all of the questions for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+const questions = [
+  { title: 'What does "NaN" stand for?',
+  choices: ["not a number", "Batman", "my Nana", "it's that bread you eat with curry"],
+  answer: "not a number"
+  },
+  { title: 'What is the most likely reason someone might use "console.log()"?',
+  choices: ["practice user snippets", "check the result of something they otherwise wouldn't be able to see", "secret spy messages"],
+  answer: "check the result of something they otherwise wouldn't be able to see"
+  },
+  { title: 'Where do you start counting when programming?',
+  choices: ["1", "0"],
+  answer: "0"
+  },
+  { title: 'What is a "Serif" font and where is it best for reading?',
+  choices: ["A Serif font has no extra little lines, and is best for screen reading",
+    "A Serif font has extra little lines, and is best for screen reading",
+    "A Serif font has no extra little lines, and is best for printed materials",
+    "A Serif font has extra little lines, and is best for printed materials"],
+  answer: "A Serif font has extra little lines, and is best for printed materials"
+  },
+  { title: 'What are the most common grid sizes for websites?',
+  choices: ["6 and 12", "9", "6 and 24", "12 and 24"],
+  answer: "12 and 24"
+  },
+  { title: 'When should you use capital letters vs. dashes?',
+  choices: ["capitals in HTML and dashes in HTML",
+  "capitals in JS and dashes in JS",
+  "capitals in HTML and dashes in JS",
+  "capitals in JS and dashes in HTML"],
+  answer: "capitals in JS and dashes in HTML"
+  },
+  { title: 'How do you say one value is the same as something else in JavaScript?',
+  choices: ["=", "==", "==="],
+  answer: "==="
+  },
+  { title: 'What does ".querySelector" do?',
+  choices: ["Goes and finds the value that has...", "Goes and gets this Id", "Goes and get all of the values so you can pick the one you want"],
+  answer: "Goes and finds the value that has..."
+  },
+  { title: 'What is jQuery?',
+  choices: ["it is an alternative to JavaScript", "it is a JavaScrip Library"],
+  answer: "it is a JavaScrip Library"
+  },
+  { title: 'How would you reference to a cousin Node?',
+  choices: ["Node.cousinNode", "Node.cousin", "cousinNode", "cousin Nodes aren't a thing"],
+  answer: "cousin Nodes aren't a thing"
+  },
+];
